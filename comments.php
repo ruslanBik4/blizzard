cb14f0d0d7cb5786d938277948285ba6eeb1cfad
ruslan-bik <ruslan-bik@yandex.ru>
2016-06-04 13:46:06 +0300

    php

---
+++ comments.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rus
+ * Date: 04.06.16
+ * Time: 13:27
+ */
+
+ const USER_RIGHT = 'right of fine live';
+
+ $name = 'George';
+
+ $age = 42;
+
+ $isEmploument = true;
+
+ echo  "Man name is <b>$name</b>, he'r age = $age USER_RIGHT <br>" . PHP_EOL;
+
+$name = 'Ivan';
+
+$age = 22;
+
+$isEmploument = false;
+
+echo  "Man name is $name, he'r age = $age " . USER_RIGHT . PHP_EOL;
+
+$name = 'Marie';
+
+$age = 18;
+
+$isEmploument = true;
+
+echo  " <br> Man name is $name, he'r age = $age ";
+
+?>
+
+<div>
+    <p>Это  учебный пример вывода из скрипта</p>
+</div>
